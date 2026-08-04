5ae42e6711faff109be3eb44ae64816221288ab9
apple <apple@email.com>
2026-08-04 23:30:31 +0300

    feat:  config file getting via param

--- launch/stereo_launch.py
+++ launch/stereo_launch.py
@@ -2,15 +2,13 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-import os
 
 def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('node_name', default_value='stereo_node'),
         DeclareLaunchArgument('voc_file', default_value=''),
         DeclareLaunchArgument('settings_path', default_value=''),
-        DeclareLaunchArgument('sub_config', default_value='/stereo_py_driver/experiment_settings'),
-        DeclareLaunchArgument('pub_ack', default_value='/stereo_py_driver/exp_settings_ack'),
+        DeclareLaunchArgument('config_name', default_value='EuRoC'),
         DeclareLaunchArgument('img_left', default_value='/stereo_py_driver/img_left_msg'),
         DeclareLaunchArgument('img_right', default_value='/stereo_py_driver/img_right_msg'),
         DeclareLaunchArgument('timestep', default_value='/stereo_py_driver/timestep_msg'),
@@ -24,8 +22,7 @@ def generate_launch_description():
                 {'node_name_arg': LaunchConfiguration('node_name')},
                 {'voc_file_arg': LaunchConfiguration('voc_file')},
                 {'settings_file_path_arg': LaunchConfiguration('settings_path')},
-                {'sub_experiment_config_topic': LaunchConfiguration('sub_config')},
-                {'pub_config_ack_topic': LaunchConfiguration('pub_ack')},
+                {'config_file_name': LaunchConfiguration('config_name')},
                 {'sub_img_left_topic': LaunchConfiguration('img_left')},
                 {'sub_img_right_topic': LaunchConfiguration('img_right')},
                 {'sub_timestep_topic': LaunchConfiguration('timestep')},
--- src/stereo_example.cpp
+++ src/stereo_example.cpp
@@ -1,7 +1,7 @@
 /*
 * Originally adapted from ORB-SLAM3: Examples/ROS/src/ros_stereo.cc
 * Author: Azmyin Md. Kamal
-* Version: 1.0
+* Version: 1.1
 * Date: 01/01/2024
 * Compatible for ROS2 Humble
 */
@@ -18,8 +18,7 @@ StereoMode::StereoMode() : Node("stereo_node_cpp")
     this->declare_parameter("node_name_arg", "stereo_node");
     this->declare_parameter("voc_file_arg", homeDir + "/" + packagePath + "orb_slam3/Vocabulary/ORBvoc.txt.bin");
     this->declare_parameter("settings_file_path_arg", homeDir + "/" + packagePath + "orb_slam3/config/Stereo/");
-    this->declare_parameter("sub_experiment_config_topic", "/stereo_py_driver/experiment_settings");
-    this->declare_parameter("pub_config_ack_topic", "/stereo_py_driver/exp_settings_ack");
+    this->declare_parameter("config_file_name", "EuRoC"); // Added parameter for config file
     this->declare_parameter("sub_img_left_topic", "/stereo_py_driver/img_left_msg");
     this->declare_parameter("sub_img_right_topic", "/stereo_py_driver/img_right_msg");
     this->declare_parameter("sub_timestep_topic", "/stereo_py_driver/timestep_msg");
@@ -28,8 +27,7 @@ StereoMode::StereoMode() : Node("stereo_node_cpp")
     nodeName = this->get_parameter("node_name_arg").as_string();
     vocFilePath = this->get_parameter("voc_file_arg").as_string();
     settingsFilePath = this->get_parameter("settings_file_path_arg").as_string();
-    subexperimentconfigName = this->get_parameter("sub_experiment_config_topic").as_string();
-    pubconfigackName = this->get_parameter("pub_config_ack_topic").as_string();
+    std::string configFileName = this->get_parameter("config_file_name").as_string();
     subImgLeftMsgName = this->get_parameter("sub_img_left_topic").as_string();
     subImgRightMsgName = this->get_parameter("sub_img_right_topic").as_string();
     subTimestepMsgName = this->get_parameter("sub_timestep_topic").as_string();
@@ -37,13 +35,14 @@ StereoMode::StereoMode() : Node("stereo_node_cpp")
     RCLCPP_INFO(this->get_logger(), "nodeName %s", nodeName.c_str());
     RCLCPP_INFO(this->get_logger(), "voc_file %s", vocFilePath.c_str());
 
-    expConfig_subscription_ = this->create_subscription<std_msgs::msg::String>(subexperimentconfigName, 1, std::bind(&StereoMode::experimentSetting_callback, this, _1));
-    configAck_publisher_ = this->create_publisher<std_msgs::msg::String>(pubconfigackName, 10);
+    // Initialize VSLAM directly using parameters
+    initializeVSLAM(configFileName);
+
     subImgLeftMsg_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(subImgLeftMsgName, 1, std::bind(&StereoMode::ImgLeft_callback, this, _1));
     subImgRightMsg_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(subImgRightMsgName, 1, std::bind(&StereoMode::ImgRight_callback, this, _1));
     subTimestepMsg_subscription_ = this->create_subscription<std_msgs::msg::Float64>(subTimestepMsgName, 1, std::bind(&StereoMode::Timestep_callback, this, _1));
 
-    RCLCPP_INFO(this->get_logger(), "Waiting to finish handshake ......");
+    RCLCPP_INFO(this->get_logger(), "Waiting for image data ......");
 }
 
 StereoMode::~StereoMode()
@@ -51,36 +50,21 @@ StereoMode::~StereoMode()
     if(pAgent) pAgent->Shutdown();
 }
 
-void StereoMode::experimentSetting_callback(const std_msgs::msg::String& msg){
-    bSettingsFromPython = true;
-    experimentConfig = msg.data.c_str();
-    
-    RCLCPP_INFO(this->get_logger(), "Configuration YAML file name: %s", experimentConfig.c_str());
-
-    auto message = std_msgs::msg::String();
-    message.data = "ACK";
-    configAck_publisher_->publish(message);
-
-    initializeVSLAM(experimentConfig);
-}
-
-void StereoMode::initializeVSLAM(std::string& configString){
+void StereoMode::initializeVSLAM(std::string configName){
     if (vocFilePath == "file_not_set" || settingsFilePath == "file_not_set")
     {
         RCLCPP_ERROR(get_logger(), "Please provide valid voc_file and settings_file paths");       
         rclcpp::shutdown();
     } 
     
-    settingsFilePath = settingsFilePath.append(configString);
-    settingsFilePath = settingsFilePath.append(".yaml");
+    std::string fullSettingsPath = settingsFilePath + configName + ".yaml";
 
-    RCLCPP_INFO(this->get_logger(), "Path to settings file: %s", settingsFilePath.c_str());
+    RCLCPP_INFO(this->get_logger(), "Path to settings file: %s", fullSettingsPath.c_str());
     
     sensorType = ORB_SLAM3::System::STEREO;
     enablePangolinWindow = true;
-    enableOpenCVWindow = true;
     
-    pAgent = new ORB_SLAM3::System(vocFilePath, settingsFilePath, sensorType, enablePangolinWindow);
+    pAgent = new ORB_SLAM3::System(vocFilePath, fullSettingsPath, sensorType, enablePangolinWindow);
     std::cout << "StereoMode node initialized" << std::endl;
 }
 
